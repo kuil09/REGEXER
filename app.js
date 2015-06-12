d18a8f7fd6334e5837d717e0a4ec59bb92b81992
Hwang, Geon Gu <under_09@naver.com>
2015-06-12 16:09:30 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,67 @@
-//참조링크 : http://jesusjzp.github.io/blog/2014/04/15/nodejs-read-file/
 
+var glob = require('glob');
 var fs = require('fs');
+var bufStr = null;
+var bufArr = [];
+var regexArr = [];
+var dir = [];
 
-var bufferString;	
-var bufferStringArr;	
-
-
-fs.readFile( __dirname + '/test.txt', function (err, data) {
-	if (err) {  
-		throw err;
+//정규식 패턴 생성을 위함
+var buildRegex = function (curArray) {
+	for(i=0; curArray.length > i; i++) {
+		curArray[i] = new RegExp(curArray[i].replace(/[\n\r]/g, ''), 'gi');	//읽어 온 변수에서 캐리지 리턴 값을 없애 정규식 생성
 	}
-	
-	bufferString = data.toString();			//텍스트 문서를 변수에 담습니다.
-	bufferStringArr = bufferString.split('\n');	//변수 내용을 개행문자 기준으로 나누어 배열에 담게 됩니다.
-	
-	console.log(bufferStringSplit);
+}
 
+//정규식 조회 조건을 포함하는 문서
+var readPattern = function() {
+	fs.readFile( __dirname + '/regex.ptn', function (err, data) {
+		if (err) {  
+			throw err;
+		}
+
+		bufStr = data.toString();
+		regexArr = bufStr.split('\n');		//이때는 단순한 문자 배열
+		buildRegex(regexArr);				//이제는 정규식을 포함한 배열
+		
+	});
+}
+
+//검색 대상 디렉토리 문서
+var searchFile = function (filePath) {
+	fs.readFile(filePath, function (err, data) {
+		if (err) {  
+			throw err;
+		}
+
+		bufStr = data.toString();
+		bufArr = bufStr.split('\n');
+		burStr = null;
+		
+		searchLine(regexArr, bufArr);
+	});
+}
+
+//정규식 패턴에 해당하는 라인 찾아서 콘솔에 출력
+var searchLine = function (regexArr, lineArr) {
+	
+	for(j=0; regexArr.length > j; j++) {
+		for(i=0; lineArr.length > i; i++) {
+			lineArr[i].search(regexArr[j]) > -1 ? console.log( 'line#:' + i + '\tregexPtn: ' + regexArr[j] + '\ncontent: ' + lineArr[i] + '\n=========================') : '';
+		}
+	}
+}
+
+glob( __dirname + "/*.txt", function (er, files) {
+	dir = files;
+
+	readPattern();
+
+	for(k=0; dir.length > k; k++) {
+	searchFile(dir[k]);
+}
 });
+
 
 
 
