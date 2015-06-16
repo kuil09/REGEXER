4b6157754f0f4b32c343fd76dce63594f145f7fa
Hwang, Geon Gu <under_09@naver.com>
2015-06-16 17:06:24 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+/* ACTION_REQUIRED 라고 지정된 행에 존재하는 변수나 경로는 사용자 환경에 맞추어 수정하시면 됩니다.
+*  아래의 regex.ptn이라는 문서는 app.js와 동일 경로 상에 존재해야 하며 라인 단위로 정규식을 넣어주시면 됩니다.
+*  맨 아래쪽의 glob 모듈을 사용하는 부분에 대해서는 검색 대상인 경로를 확인하셔야 합니다. 
+*  OS별로 역슬래시와 슬래시의 차이가 있습니다. (UNIX계열과 윈도우 계열의 차이입니다.)
+*  알려진 이슈 : 1. 인코딩 차이에 대해서는 아직 해결한 바가 없습니다. UTF-8을 전제로 작업했습니다.
+*                2. 마이크로 소프트의 엑셀에서는 CSV 파일의 UTF-8 인코딩을 지원하지 않습니다. 필요하시면 별도로 수정하셔서 사용하세요.
+*/
+
 
 var glob = require('glob');
 var fs = require('fs');
-var wstream = fs.createWriteStream('matched.csv');	//확인된 패턴이 matchd.csv 파일로 출력
+var wstream = fs.createWriteStream('matched.csv');	//ACTION_REQUIRED 확인된 패턴이 matchd.csv 파일로 출력
 var bufStr = null;
 var bufArr = [];
 var regexArr = [];
@@ -14,7 +22,7 @@ var buildRegex = function (curArray) {
 	}
 }
 
-//정규식 조회 조건을 포함하는 문서 regex.ptn 문서에 포함되며 특수문자의 경우 escape 처리가 필요
+//ACTION_REQUIRED 정규식 조회 조건을 포함하는 문서 regex.ptn 문서에 포함되며 특수문자의 경우 escape 처리가 필요
 var readPattern = function() {
 	fs.readFile( __dirname + '/regex.ptn', {encoding: 'utf8'}, function (err, data) {
 		if (err) {  
@@ -54,7 +62,7 @@ var searchLine = function (filePath, regexArr, lineArr) {
 	}
 }
 
-//조회 대상 경로. glob 모듈 사용자 설명 참조
+//ACTION_REQUIRED 조회 대상 경로. glob 모듈 사용자 설명 참조
 glob( "C:/**/*.js", function (er, files) {	
 	dir = files;
 
